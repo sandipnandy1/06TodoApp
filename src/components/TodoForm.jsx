d7f4016f045b68eb3fb92ddba54714a5c79a950a
Sandip <sandipnandy9@gmail.com>
2024-08-12 14:21:15 +0530

    Todo App ready

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,14 +1,32 @@
+import { useState } from "react";
+import { useTodo } from "../contextAPI/TodoContext";
+
 function TodoForm() {
+    const [todo, setTodo] = useState("")
+
+    const { addTodo } = useTodo()
+
+
+    const handleClick = (e) => {
+        e.preventDefault()
+
+        if(!todo) return;
+        addTodo({ id: Date.now(), todo: todo, completed: false})
+        setTodo("")
+        
+    }
     
 
     return (
-        <form  className="flex">
+        <form className="flex">
             <input
                 type="text"
                 placeholder="Write Todo..."
                 className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+                onChange={e => setTodo(e.target.value)}
+                value ={todo}
             />
-            <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
+            <button onClick={handleClick} type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
                 Add
             </button>
         </form>
